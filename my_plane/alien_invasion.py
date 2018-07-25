d2fd07e5a4f4005400e3a99f731b42decf4ee9d6
lewisliuyi <1226228799@qq.com>
2018-07-26 00:05:15 +0800

    tt

--- my_plane/alien_invasion.py
+++ my_plane/alien_invasion.py
@@ -16,12 +16,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 开始主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
